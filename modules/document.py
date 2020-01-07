bc55e06437c171c040ed6ab2e4e796de1ddc5dcb
klimeto <tomas.kliment@gmail.com>
2020-01-07 23:32:25 +0100

    add methods to DeleteDocument to delete sms_form and minutes

--- modules/document.py
+++ modules/document.py
@@ -80,7 +80,7 @@ class UploadDocument(Resource):
         if not self.allowed_file(file.filename):
             return Response('{"message":"file extension forbidden"}', mimetype='application/json', status=400)
         filename = secure_filename(file.filename)
-        document_folder = os.path.join(UPLOAD_FOLDER, inspection_type, hashid)
+        document_folder = os.path.join(UPLOAD_FOLDER, inspection_type, hashid, doc_type)
         if not os.path.exists(document_folder):
             os.makedirs(document_folder)
         filepath = os.path.join(document_folder, filename)
@@ -212,6 +212,36 @@ class DeleteDocument(Resource):
         return update_data
 
     @auth.login_required
+    def delete_inspection_sms_form(self, inspection_type, oid):
+        id_person_role = self.person_class.getPersonRoleId(g.user)
+        id_user = str(id_person_role[0][0][0])
+        # id_user = 100
+        updated_date = datetime.datetime.now().strftime('%Y-%m-%d')
+        if inspection_type == 'specific':
+            SQL = "UPDATE cide_specific_inspection SET last_update=%s, id_user=%s, sms_form = NULL WHERE id_specific_inspection = %s RETURNING id_specific_inspection"
+        else:
+            SQL = "UPDATE cide_coordinated_inspection SET last_update=%s, id_user=%s, sms_form = NULL WHERE id_coordinated_inspection = %s RETURNING id_coordinated_inspection"
+        self.connection.connect()
+        update_data = self.connection.query(sql=SQL, data=(updated_date, id_user, oid), fetch=False)
+        self.connection.close()
+        return update_data
+
+    @auth.login_required
+    def delete_inspection_minutes(self, inspection_type, oid):
+        id_person_role = self.person_class.getPersonRoleId(g.user)
+        id_user = str(id_person_role[0][0][0])
+        # id_user = 100
+        updated_date = datetime.datetime.now().strftime('%Y-%m-%d')
+        if inspection_type == 'specific':
+            SQL = "UPDATE cide_specific_inspection SET last_update=%s, id_user=%s, minutes = NULL WHERE id_specific_inspection = %s RETURNING id_specific_inspection"
+        else:
+            SQL = "UPDATE cide_coordinated_inspection SET last_update=%s, id_user=%s, minutes = NULL WHERE id_coordinated_inspection = %s RETURNING id_coordinated_inspection"
+        self.connection.connect()
+        update_data = self.connection.query(sql=SQL, data=(updated_date, id_user, oid), fetch=False)
+        self.connection.close()
+        return update_data
+
+    @auth.login_required
     def delete(self):
         doc_type = request.args.get('document')
         inspection_type = request.args.get('type')
@@ -219,9 +249,16 @@ class DeleteDocument(Resource):
         if not doc_type or not inspection_type or not inspection_hash:
             return Response(status=400)
         inspection_oid = (hashids.decode(inspection_hash))[0]
-        update_database = self.delete_inspection_final_report(inspection_type, inspection_oid)
-        if update_database:
-            result = '{"updated":"' + hashids.encode(update_database[0][0]) + '"}'
+        if doc_type == 'final_report':
+            delete_document = self.delete_inspection_final_report(inspection_type, inspection_oid)
+        elif doc_type == 'sms_form':
+            delete_document = self.delete_inspection_sms_form(inspection_type, inspection_oid)
+        elif doc_type == 'minutes':
+            delete_document = self.delete_inspection_minutes(inspection_type, inspection_oid)
+        else:
+            return Response(status=400)
+        if delete_document:
+            result = '{"updated":"' + hashids.encode(delete_document[0][0]) + '"}'
         else:
             result = '{"updated": 0}'
         return Response(result, mimetype='application/json')
